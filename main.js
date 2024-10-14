0cee7a03402b3498f3a77f2ad2616e7c2eb60900
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 16:19:55 -0300

    ALL IN!!!!!!!!!!

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ const botaoA = document.querySelector("button");
 
 botaoA.onclick = () => {
   var nome = prompt("Qual seu nome?",null);
+  document.getElementById("name_var").textContent = nome;
 }
