f2942a190f4c8cabba9fa6d94833efbcd6c03e84
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 20:54:29 -0300

    message

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botaoA = document.querySelector("button");
-
+let json = require("./players.json");
 botaoA.onclick = () => {
   var nome = prompt("Qual seu nome?",null);
-  document.querySelector("abcd").textContent = nome;
+  document.querySelector("abcd").textContent = JSON.stringify(json);
 }
