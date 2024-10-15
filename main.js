85853435fe254ef53a827782925db7581785a131
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 22:32:52 -0300

    çççç

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const botaoA = document.querySelector("button");
 const td11 = document.getElementById("abcd");
 let header2 = document.querySelector("h2");
 header2.textContent = "Não sei oq ta acontecendo!";
+/*
 let json_var =await fetch("players.JSON").json();
 header2.textContent = json_var.players[0].nome;
 /*
