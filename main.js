8adb51fa5451083bd40e45aeb787de5a7593dacb
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 21:39:26 -0300

    Esqueci da mensage

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const td11 = document.getElementById("abcd");
 //let json_var =await fetch(new Request("./players.JSON")).json();
 
 botaoA.onclick = () => {
-  var nome = prompt("Qual seu nome?",null);
+  //var nome = prompt("Qual seu nome?",null);
   td11.textContent = JSON.stringify(json);
 }
