12323fb9d52ace0d7f37d6f817db9798cf45145e
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 22:04:49 -0300

    alterado URL pra URl do github

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botaoA = document.querySelector("button");
 const td11 = document.getElementById("abcd");
-let json_var =await fetch(new Request("./players.JSON")).json();
+let json_var =await fetch(new Request("https://github.com/Noobs0nes/Ficha-DND5E/blob/main/players.JSON")).json();
 
 botaoA.onclick = () => {
   //var nome = prompt("Qual seu nome?",null);
