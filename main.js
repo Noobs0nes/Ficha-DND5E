3ba4eabd9055587e42b3c3865e5a688c35336a2a
Noobs0nes <Noobs0nes@gmail.com>
2024-10-15 11:13:09 -0300

    RETORNA! :)

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-var mydata = JSON.parse(data);
- alert(mydata[0].name);
- alert(mydata[0].age);
- alert(mydata[1].name);
- alert(mydata[1].age);
+const botaoA = document.querySelector("button");
+const td11 = document.getElementById("abcd");
+//import data from "./players.JSON";
+let header2 = document.querySelector("h2");
+header2.textContent = "Não sei oq ta acontecendo!";
+botaoA.onclick = () => {
+  alert("Botão clicado!")
+  let json_var = data;
+  alert("Deu boa?")
+  header2.textContent = json_var.players[0].nome;
+  //var nome = prompt("Qual seu nome?",null);
+  alert(json_var);
+  td11.textContent = JSON.stringify(json_var);
+}
