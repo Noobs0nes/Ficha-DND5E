be3a2fcc44ccda59822fe9917362256d0b49ba67
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 21:15:42 -0300

    ???

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ let json = require("./players.json");
 let json =await  Response(await fetch(new Request("./players.JSON")));
 botaoA.onclick = () => {
   var nome = prompt("Qual seu nome?",null);
-  botaoA.textContent = JSON.stringify(json);
+  botaoA.textContent =  "a"//JSON.stringify(json);
 }
