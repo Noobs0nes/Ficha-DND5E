2931f86e5a30cc489c0b1e4c5fa4a194f933ab68
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 22:37:11 -0300

    4

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const td11 = document.getElementById("abcd");
 let header2 = document.querySelector("h2");
 header2.textContent = "Não sei oq ta acontecendo!";
 botaoA.onclick = () => {
+  alert("Botão clicado!")
   let json_var = fetch("players.JSON").json();
   header2.textContent = json_var.players[0].nome;
   //var nome = prompt("Qual seu nome?",null);
