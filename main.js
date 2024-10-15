0cd3fb0f927e69042286d50bc097c45964736ecf
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 22:00:34 -0300

    DNV KRL

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const botaoA = document.querySelector("button");
 const td11 = document.getElementById("abcd");
-//let json_var =await fetch(new Request("./players.JSON")).json();
+let json_var =await fetch(new Request("./players.JSON")).json();
 
 botaoA.onclick = () => {
   //var nome = prompt("Qual seu nome?",null);
-  alert(json);
-  td11.textContent = JSON.stringify(json);
+  alert(json_var);
+  td11.textContent = JSON.stringify(json_var);
 }
