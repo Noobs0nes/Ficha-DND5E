35c36b35e3ac1c809af87400824ddd42c560c566
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 22:35:43 -0300

    a

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ const botaoA = document.querySelector("button");
 const td11 = document.getElementById("abcd");
 let header2 = document.querySelector("h2");
 header2.textContent = "Não sei oq ta acontecendo!";
-/*
-let json_var =await fetch("players.JSON").json();
-header2.textContent = json_var.players[0].nome;
-/*
 botaoA.onclick = () => {
+  let json_var = fetch("players.JSON").json();
+  header2.textContent = json_var.players[0].nome;
   //var nome = prompt("Qual seu nome?",null);
   alert(json_var);
   td11.textContent = JSON.stringify(json_var);
-}*/
+}
