038ebbde1a6cdd966ac79790428a1a027e6c24bf
Noobs0nes <Noobs0nes@gmail.com>
2024-10-14 22:15:02 -0300

    7

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const botaoA = document.querySelector("button");
 const td11 = document.getElementById("abcd");
-let json_var =await fetch(new Request("https://github.com/Noobs0nes/Ficha-DND5E/blob/main/players.JSON")).json();
-
+let json_var =await fetch("/players.JSON").json();
+let header2 = document.createElement("h2");
+header2.textContent = json_var.players[0].nome;
+/*
 botaoA.onclick = () => {
   //var nome = prompt("Qual seu nome?",null);
   alert(json_var);
   td11.textContent = JSON.stringify(json_var);
-}
+}*/
