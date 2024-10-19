eed6786e112ea74d4d9581ca46bfedc520da7afb
Noobs0nes <Noobs0nes@gmail.com>
2024-10-19 12:47:15 -0300

    HEh.

--- players/player.js
+++ players/player.js
@@ -2,6 +2,5 @@ let host =  document.location.host ;
 let prefix = "http://" + host +"/players/";
 let sufix = ".html";
 let title = document.location.href.replace(prefix,'').replace(sufix,'').toUpperCase();
-document.createElement("h1").textContent = prefix
-
-document.querySelector("title").innerText = title
+document.querySelector("title").innerText = title;
+document.getElementById("tituloPagina").textContent = title;
