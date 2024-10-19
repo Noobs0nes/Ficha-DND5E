f63cf14ed530ec85b2ff16259f7ed62c95009357
Noobs0nes <Noobs0nes@gmail.com>
2024-10-19 18:33:21 -0300

    last?

---
+++ portrait/portrait.js
@@ -0,0 +1,8 @@
+import css_call from "./css_call.js"
+function alteraVida(value){
+    document.getElementById("vidaAtual").textContent = value;
+    31
+    css_call.changeCssVar(percent);
+
+    
+}
