738ba51725ee9c7056ed83f5f9783ba2c2a3d358
Noobs0nes <Noobs0nes@gmail.com>
2024-10-20 01:07:30 -0300

    asd
    qq

--- portrait/portrait.js
+++ portrait/portrait.js
@@ -1,9 +1,11 @@
-import css_call from "./css_call.js"
 function alteraVida(value){
     document.getElementById("vidaAtual").textContent = value;
     let total = document.getElementById("vidaTotal").textContent;
     let percent = Number(value)/Number(total)*100
-    css_call.changeCssVar(percent);
-
-    
+    document.documentElement.style.setProperty("--WIDTHVIDA",percent+"%");   
+}
+let b = document.querySelector("button")
+b.onclick = ()=>{
+    let vida =prompt("vida");
+    alteraVida(vida);    
 }
